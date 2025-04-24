384f6ba2ccb3c3b8a9ab607612aa6f06bbda8617
protocol_1903 <fisher.braydon@yahoo.com>
2025-04-24 10:46:57 -0700

    Fixed a crash when undoing a deconstruction in editor mode and the player's hand is empty

--- control.lua
+++ control.lua
@@ -217,7 +217,7 @@ function handle(event)
   if not player or not storage.tomwub[player.index] then return end
 
   -- if player just placed last item, then signal to script to update hand again
-  if player.is_cursor_empty() and storage.tomwub[player.index].item:sub(1,7) == "tomwub-" and storage.tomwub[player.index].count == 1 then
+  if player.is_cursor_empty() and storage.tomwub[player.index].item and storage.tomwub[player.index].item:sub(1,7) == "tomwub-" and storage.tomwub[player.index].count == 1 then
     storage.tomwub[player.index].count = -1
 
     -- set ghost cursor
@@ -417,7 +417,7 @@ function handle(event)
     if not player then return end
   
     -- if player just placed last item, then signal to script to update hand again
-    if player.is_cursor_empty() and storage.tomwub[player.index].item:sub(1,7) == "tomwub-" and storage.tomwub[player.index].count == 1 then
+    if player.is_cursor_empty() and storage.tomwub[player.index].item and storage.tomwub[player.index].item:sub(1,7) == "tomwub-" and storage.tomwub[player.index].count == 1 then
       storage.tomwub[player.index].count = -1
   
       -- set ghost cursor
