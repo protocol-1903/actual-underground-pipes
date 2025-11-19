71ed9382e2c38e437e509ccee4a5d529e56b8d20
protocol-1903 <fisher.braydon@yahoo.com>
2025-11-18 19:12:57 -0700

    Abstracted adjust_recipe, ptg_visualization, ptg_visualizations, base_visualization, and dirmap to xutil

--- util.lua
+++ util.lua
@@ -41,4 +41,156 @@ xutil.reformat = function(spritesheet)
   end
 end
 
+xutil.ptg_visualization = function(underground)
+  return {
+    north = {
+      filename = underground and "__the-one-mod-with-underground-bits__/graphics/visualization.png" or "__base__/graphics/entity/pipe-to-ground/visualization.png",
+      priority = "extra-high",
+      x = 64,
+      width = 64,
+      height = 64,
+      scale = 0.5,
+      shift = underground and util.by_pixel(0, xutil.downshift) or nil,
+      flags = {"icon"}
+    },
+    south = {
+      filename = underground and "__the-one-mod-with-underground-bits__/graphics/visualization.png" or "__base__/graphics/entity/pipe-to-ground/visualization.png",
+      priority = "extra-high",
+      x = 192,
+      width = 64,
+      height = 64,
+      scale = 0.5,
+      shift = underground and util.by_pixel(0, xutil.downshift) or nil,
+      flags = {"icon"}
+    },
+    west = {
+      filename = underground and "__the-one-mod-with-underground-bits__/graphics/visualization.png" or "__base__/graphics/entity/pipe-to-ground/visualization.png",
+      priority = "extra-high",
+      x = 256,
+      width = 64,
+      height = 64,
+      scale = 0.5,
+      shift = underground and util.by_pixel(0, xutil.downshift) or nil,
+      flags = {"icon"}
+    },
+    east = {
+      filename = underground and "__the-one-mod-with-underground-bits__/graphics/visualization.png" or "__base__/graphics/entity/pipe-to-ground/visualization.png",
+      priority = "extra-high",
+      x = 128,
+      width = 64,
+      height = 64,
+      scale = 0.5,
+      shift = underground and util.by_pixel(0, xutil.downshift) or nil,
+      flags = {"icon"}
+    }
+  }
+end
+
+xutil.ptg_visualizations = {
+  north = {
+    layers = {
+      xutil.ptg_visualization(true).south,
+      xutil.ptg_visualization().north,
+    }
+  },
+  east = {
+    layers = {
+      xutil.ptg_visualization(true).west,
+      xutil.ptg_visualization().east,
+    }
+  },
+  south = {
+    layers = {
+      xutil.ptg_visualization(true).north,
+      xutil.ptg_visualization().south,
+    }
+  },
+  west = {
+    layers = {
+      xutil.ptg_visualization(true).east,
+      xutil.ptg_visualization().west,
+    }
+  },
+}
+
+xutil.base_visualisation = {
+  north = {layers = {
+    xutil.ptg_visualization().north
+  }},
+  east = {layers = {
+    xutil.ptg_visualization().east
+  }},
+  south = {layers = {
+    xutil.ptg_visualization().south
+  }},
+  west = {layers = {
+    xutil.ptg_visualization().west
+  }},
+}
+
+xutil.dirmap = {
+  [0] = "north",
+  "east",
+  "south",
+  "west"
+}
+
+xutil.adjust_recipes = function(u)
+  -- if recipe exists
+  if not mods["bztin"] then
+    -- fix normal recipes
+    for _, recipe in pairs{
+      u,
+      "casting-" .. u
+    } do
+      -- if recipe exists
+      if data.raw.recipe[recipe] then
+        -- just pipes, set to 2
+        if #data.raw.recipe[recipe].ingredients == 1 and data.raw.recipe[recipe].ingredients[1].name:find("pipe") then
+          data.raw.recipe[recipe].ingredients[1].amount = 2
+        else -- not just pipes, get rid of them
+          local ingredients = table.deepcopy(data.raw.recipe[recipe].ingredients)
+          data.raw.recipe[recipe].ingredients = {}
+          -- add ingredient if not the associated pipe
+          for _, ingredient in pairs(ingredients) do
+            if not ingredient.name:find("pipe") then
+              data.raw.recipe[recipe].ingredients[#data.raw.recipe[recipe].ingredients+1] = ingredient
+            end
+          end
+        end
+      end
+    end
+  elseif mods["bztin"] and data.raw.recipe[u] then
+    -- modify counts
+    for _, ingredient in pairs(data.raw.recipe[u].ingredients) do
+      if data.raw.pipe[ingredient.name] and ingredient.amount > 2 then
+        ingredient.amount = 2 -- if a pipe, set amount to 2
+      end
+    end
+      
+    -- if recycling recipe exists
+    if data.raw.recipe[u .. "-recycling"] then
+      -- add result if not the associated pipe
+      for _, result in pairs(data.raw.recipe[u .. "-recycling"].results) do
+        if data.raw.pipe[result.name] and result.amount > 2 then
+          result.amount = 1
+          result.probability = 0.5
+        end
+      end
+    end
+  end
+      
+  -- if recycling recipe exists
+  if data.raw.recipe[u .. "-recycling"] then
+    local results = table.deepcopy(data.raw.recipe[u .. "-recycling"].results)
+    data.raw.recipe[u .. "-recycling"].results = {}
+    -- add result if not the associated pipe
+    for _, result in pairs(results) do
+      if not result.name:find("pipe") then
+        table.insert(data.raw.recipe[u .. "-recycling"].results, result)
+      end
+    end
+  end
+end
+
 return xutil
