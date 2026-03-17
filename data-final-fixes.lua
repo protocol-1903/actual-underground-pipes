d45d8cd161d80451778751e653314caaea5b4015
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-03-17 14:37:21 -0700

    move functions to xutil, copy prototypes instead of making them from scratch

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -14,158 +14,13 @@ end
 for p, pipe in pairs(data.raw.pipe) do
   for u, underground in pairs(data.raw["pipe-to-ground"]) do
     if u:sub(1,-11) == p and not underground.ignore_by_tomwub then
-      underground.solved_by_tomwub = true
-      local underground_collision_mask, layer, connection_category
-      -- the underground name matches with the pipe name
-      -- also only runs this chunk of code once per supported underground
-      for _, pipe_connection in pairs(underground.fluid_box.pipe_connections) do
-        if pipe_connection.connection_type == "underground" then
-          -- make the underground a fake underground
-          pipe_connection.connection_type = "normal"
-          pipe_connection.max_underground_distance = nil
-          -- set the filter to the psuedo underground pipe name
-          if not mods["no-pipe-touching"] then
-            pipe_connection.connection_category = "tomwub-underground"
-          elseif not underground.npt_compat then
-            pipe_connection.connection_category = "tomwub-" .. p .. "-underground"
-          elseif underground.npt_compat.tag then
-            pipe_connection.connection_category = "tomwub-" .. underground.npt_compat.mod .. "-" .. underground.npt_compat.tag .. "-underground"
-          elseif underground.npt_compat.override then
-            pipe_connection.connection_category = "tomwub-" .. underground.npt_compat.override .. "-underground"
-          end
-          -- save collision mask for later
-          underground_collision_mask = pipe_connection.underground_collision_mask or {layers = {}}
-          connection_category = pipe_connection.connection_category
-          layer = settings.startup["npt-tomwub-weaving"].value and pipe_connection.connection_category or "tomwub-underground"
-        end
-      end
-
-      -- assign new visualizations for the pipe-to-ground
-      underground.visualization = xutil.ptg_visualizations
-
-      -- set heating enrergy of pipe-to-ground to that of the pipe
+      local mask, layer, connection_category = xutil.adjust_ptg(underground, p)
       underground.heating_energy = pipe.heating_energy
-
-      -- update collision mask
-      if not underground.collision_mask then
-        underground.collision_mask = {
-          layers = {
-            is_lower_object = true,
-            water_tile = true,
-            floor = true,
-            transport_belt = true,
-            item = true,
-            car = true,
-            meltable = true
-          }
-        }
-      end
-
-      -- set the collision mask to the connection_category collected earlier
-      underground.collision_mask.layers[layer] = true
+      xutil.make_tomwub_variant(pipe, mask, layer, connection_category)
+      xutil.adjust_recipes(u)
 
       -- save the tag for later use with assembling machines
       tags[#tags+1] = connection_category
-
-      -- create new item, entity, and collision layer
-      data.extend{
-        {
-          type = "item",
-          name = "tomwub-" .. p,
-          icon = pipe.icon,
-          icon_size = pipe.icon_size,
-          icons = pipe.icons or {{
-            icon = pipe.icon or data.raw.pipe.pipe.icon,
-            icon_size = pipe.icon_size or data.raw.pipe.pipe.icon_size,
-          }},
-          place_result = "tomwub-" .. p,
-          flags = {"only-in-cursor"},
-          stack_size = data.raw.item[p].stack_size
-        },
-        {
-          type = "pipe",
-          name = "tomwub-" .. p,
-          icon = pipe.icon,
-          icon_size = pipe.icon_size,
-          icons = pipe.icons or {{
-            icon = pipe.icon or data.raw.pipe.pipe.icon,
-            icon_size = pipe.icon_size or data.raw.pipe.pipe.icon_size,
-          }},
-          localised_name = {"entity-name.tomwub-underground", pipe.localised_name or {"entity-name." .. pipe.name}},
-          fluid_box = table.deepcopy(pipe.fluid_box),
-          pictures = table.deepcopy(pipe.pictures),
-          collision_box = pipe.collision_box,
-          selection_box = pipe.selection_box,
-          collision_mask = underground_collision_mask or { layers = {} },
-          flags = {"not-upgradable", "player-creation", "placeable-neutral", "not-flammable"},
-          resistances = underground_total_resistances,
-          hide_resistances = true,
-          horizontal_window_bounding_box = {{0,0},{0,0}},
-          vertical_window_bounding_box = {{0,0},{0,0}},
-          icon_draw_specification = table.deepcopy(pipe.icon_draw_specification or data.raw.pipe.pipe.icon_draw_specification),
-          minable = pipe.minable,
-          selection_priority = 255,
-          placeable_by = { {item = "tomwub-" .. p, count = 1}, {item = p, count = 1} },
-          is_military_target = false
-        }
-      }
-
-      -- since we can only check while in the loop
-      if settings.startup["npt-tomwub-weaving"].value and table_size(data.raw["collision-layer"]) == 55 then
-        local ptg_list = ""
-        for prototype in pairs(data.raw["pipe-to-ground"]) do
-          ptg_list = ptg_list .. "- " .. prototype .. "\n"
-        end
-        error("There are too many pipes. Please remove one of the following mods:\n" .. (
-          (mods["RGBPipes"] and "- RGB Pipes\n" or "") ..
-          (mods["pipe-tiers"] and "- Pipe Tiers\n" or "")
-        ) .. "\nOr disable the mod setting: Enable underground pipe weaving.\n\nOr remove a mod that adds some of the following:\n" .. ptg_list)
-      end
-
-      if settings.startup["npt-tomwub-weaving"].value then
-        data.extend{{
-          type = "collision-layer",
-          name = layer
-        }}
-      end
-
-      local tomwub_pipe = data.raw.pipe["tomwub-" .. p]
-      for _, pipe_connection in pairs(tomwub_pipe.fluid_box.pipe_connections) do
-        pipe_connection.connection_category = connection_category
-      end
-
-      -- set the collision mask to the connection_category collected earlier
-      tomwub_pipe.collision_mask.layers[layer] = true
-
-      -- shift everything down
-      tomwub_pipe.icon_draw_specification.shift = util.by_pixel(0, xutil.downshift)
-      xutil.reformat(tomwub_pipe.pictures)
-      tomwub_pipe.fluid_box.pipe_covers = tomwub_pipe.fluid_box.pipe_covers or table.deepcopy(pipecoverspictures())
-      xutil.reformat(tomwub_pipe.fluid_box.pipe_covers)
-
-      -- hide flow pictures
-      tomwub_pipe.pictures.gas_flow = nil
-      tomwub_pipe.pictures.low_temperature_flow = nil
-      tomwub_pipe.pictures.middle_temperature_flow = nil
-      tomwub_pipe.pictures.high_temperature_flow = nil
-
-      -- scale down the fluid icon
-      tomwub_pipe.icon_draw_specification.scale = 0.35
-
-      -- add placement visualization
-      if settings.startup["pipe-opacity"].value == 0 then
-        tomwub_pipe.radius_visualisation_specification = {
-          sprite = {
-            filename = "__the-one-mod-with-underground-bits__/graphics/placement-visualization.png",
-            size = {160, 160}
-          },
-          offset = util.by_pixel(0, xutil.downshift),
-          distance = 0.65
-        }
-      end
-
-      -- attempt to fix recipes
-      xutil.adjust_recipes(u)
     end
   end
 end
@@ -292,7 +147,7 @@ for _, type in pairs{
   "valve"
  } do
   for _, prototype in pairs(data.raw[type] or {}) do
-    if not prototype.ignore_by_tomwub then
+    if not prototype.ignore_by_tomwub and not prototype.solved_by_tomwub then
       local fluid_boxes = {}
       -- multiple fluid_boxes
       for _, fluid_box in pairs(prototype.fluid_boxes or {}) do
@@ -323,9 +178,10 @@ for _, type in pairs{
           end
         end
       end
-    else
+    elseif prototype.ignore_by_tomwub then
       log("ignoring prototype: " .. prototype.name)
       prototype.ignore_by_tomwub = nil
     end
+    prototype.solved_by_tomwub = nil
   end
 end
--- util.lua
+++ util.lua
@@ -177,4 +177,125 @@ xutil.adjust_recipes = function(u)
   end
 end
 
+xutil.adjust_ptg = function(prototype, pipe)
+  prototype.solved_by_tomwub = true
+  local underground_collision_mask, layer, connection_category
+  for _, pipe_connection in pairs(prototype.fluid_box.pipe_connections) do
+    if pipe_connection.connection_type == "underground" then
+      -- make the underground a fake underground
+      pipe_connection.connection_type = "normal"
+      pipe_connection.max_underground_distance = nil
+      -- set the filter to the psuedo underground pipe name
+      if not mods["no-pipe-touching"] then
+        pipe_connection.connection_category = "tomwub-underground"
+      elseif not prototype.npt_compat then
+        pipe_connection.connection_category = "tomwub-" .. pipe .. "-underground"
+      elseif prototype.npt_compat.tag then
+        pipe_connection.connection_category = "tomwub-" .. prototype.npt_compat.mod .. "-" .. prototype.npt_compat.tag .. "-underground"
+      elseif prototype.npt_compat.override then
+        pipe_connection.connection_category = "tomwub-" .. prototype.npt_compat.override .. "-underground"
+      end
+      -- save collision mask for later
+      underground_collision_mask = pipe_connection.underground_collision_mask or {layers = {}}
+      connection_category = pipe_connection.connection_category
+      layer = settings.startup["npt-tomwub-weaving"].value and pipe_connection.connection_category or "tomwub-underground"
+    end
+  end
+
+  prototype.visualization = xutil.ptg_visualizations
+  prototype.collision_mask = prototype.collision_mask or {
+    layers = {
+      is_lower_object = true,
+      water_tile = true,
+      floor = true,
+      transport_belt = true,
+      item = true,
+      car = true,
+      meltable = true
+    }
+  }
+
+  prototype.collision_mask.layers[layer] = true
+  -- set the collision mask to the connection_category collected earlier
+
+  return underground_collision_mask, layer, connection_category
+end
+
+xutil.make_tomwub_variant = function(pipe, mask, layer, connection_category)
+  -- create new item, entity, and collision layer
+  local item = table.deepcopy(data.raw.item[pipe.name])
+  item.name = "tomwub-" .. pipe.name
+  item.place_result = "tomwub-" .. pipe.name
+  item.flags = {"only-in-cursor"}
+
+  local u_pipe = table.deepcopy(pipe)
+  u_pipe.name = "tomwub-" .. pipe.name
+  u_pipe.localised_name = {"entity-name.tomwub-underground", pipe.localised_name or {"entity-name." .. pipe.name}}
+  u_pipe.collision_mask = mask or { layers = {} }
+  u_pipe.flags = {"not-upgradable", "player-creation", "placeable-neutral", "not-flammable"}
+  u_pipe.placeable_by = {{item = "tomwub-" .. pipe.name, count = 1}, {item = pipe.name, count = 1}}
+  u_pipe.resistances = underground_total_resistances
+  u_pipe.hide_resistances = true
+  u_pipe.horizontal_window_bounding_box = {{0,0},{0,0}}
+  u_pipe.vertical_window_bounding_box = {{0,0},{0,0}}
+  u_pipe.selection_priority = 255
+  u_pipe.is_military_target = false
+  u_pipe.fast_replaceable_group = settings.startup["npt-tomwub-weaving"].value and u_pipe.name or "tomwub-pipe"
+
+  data:extend{item, u_pipe}
+
+  -- since we can only check while in the loop
+  if settings.startup["npt-tomwub-weaving"].value and table_size(data.raw["collision-layer"]) == 55 then
+    local ptg_list = ""
+    for prototype in pairs(data.raw["pipe-to-ground"]) do
+      ptg_list = ptg_list .. "- " .. prototype .. "\n"
+    end
+    error("There are too many pipes. Please remove one of the following mods:\n" .. (
+      (mods["RGBPipes"] and "- RGB Pipes\n" or "") ..
+      (mods["pipe-tiers"] and "- Pipe Tiers\n" or "")
+    ) .. "\nOr disable the mod setting: Enable underground pipe weaving.\n\nOr remove a mod that adds some of the following:\n" .. ptg_list)
+  end
+
+  if settings.startup["npt-tomwub-weaving"].value then
+    data.extend{{
+      type = "collision-layer",
+      name = layer
+    }}
+  end
+
+  for _, pipe_connection in pairs(u_pipe.fluid_box.pipe_connections) do
+    pipe_connection.connection_category = connection_category
+  end
+
+  -- set the collision mask to the connection_category collected earlier
+  u_pipe.collision_mask.layers[layer] = true
+
+  -- shift everything down
+  u_pipe.icon_draw_specification.shift = util.by_pixel(0, xutil.downshift)
+  xutil.reformat(u_pipe.pictures)
+  u_pipe.fluid_box.pipe_covers = u_pipe.fluid_box.pipe_covers or table.deepcopy(pipecoverspictures())
+  xutil.reformat(u_pipe.fluid_box.pipe_covers)
+
+  -- hide flow pictures
+  u_pipe.pictures.gas_flow = nil
+  u_pipe.pictures.low_temperature_flow = nil
+  u_pipe.pictures.middle_temperature_flow = nil
+  u_pipe.pictures.high_temperature_flow = nil
+
+  -- scale down the fluid icon
+  u_pipe.icon_draw_specification.scale = 0.35
+
+  -- add placement visualization
+  if settings.startup["pipe-opacity"].value == 0 then
+    u_pipe.radius_visualisation_specification = {
+      sprite = {
+        filename = "__the-one-mod-with-underground-bits__/graphics/placement-visualization.png",
+        size = {160, 160}
+      },
+      offset = util.by_pixel(0, xutil.downshift),
+      distance = 0.65
+    }
+  end
+end
+
 return xutil
