7775c9dff1e8b15a7d671cf6578345d1163a06b7
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-05-23 18:38:24 -0700

    Fixed that certain mod combinations would duplicate items when the last underground pipe in the cursor is placed. Resolves https://github.com/protocol-1903/the-one-mod-with-underground-bits/issues/55

--- control.lua
+++ control.lua
@@ -444,7 +444,7 @@ local function on_built(event)
   local player = game.get_player(event.player_index)
 
   -- if player just placed last item, then signal to script to update hand again
-  if player.is_cursor_empty() and storage.tomwub[player.index].item and storage.tomwub[player.index].item:sub(1,7) == "tomwub-" and storage.tomwub[player.index].count == 1 then
+  if storage.tomwub[player.index].item and storage.tomwub[player.index].item:sub(1,7) == "tomwub-" and storage.tomwub[player.index].count == 1 then
     storage.tomwub[player.index].count = -1
 
     -- set ghost cursor
